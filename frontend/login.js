4d5381a511b313b3ef33ef6f642151136f855b4a
Alex Randle <alexrrandle2001@gmail.com>
2022-12-13 16:45:36 -0500

    start of the login process
    login stuff going

---
+++ frontend/login.js
@@ -0,0 +1,5 @@
+function login(){
+    let email = document.getElementById("email").value
+    let pass = document.getElementById("pass").value
+    window.alert(`login test info:${email} ${pass}`);
+}
